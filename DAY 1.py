66a8d4a3c3a09deeb3e3cc2ca306f263cfe81af0
abdul mannan <abdul.mannan.silant@gmail.com>
2025-09-13 21:48:17 +0530

    Learnt about loops

---
+++ DAY 1.py
@@ -0,0 +1,11 @@
+print("Hello World")
+
+# This is a program that says hello and counts to 3
+print("Hello!")        # Tells the computer to say Hello!
+print(1)               # Tells the computer to say 1
+print(2)               # Tells the computer to say 2
+print(3)               # Tells the computer to say 3
+
+# This loop counts from 1 to 5
+for number in range(1, 6):
+	print(number)
