a9c3a79b82269ba45db29c63c9e68cd40d19bfc9
abdul mannan <abdul.mannan.silant@gmail.com>
2025-09-13 21:55:35 +0530

    Added my first changes

--- DAY 1.py
+++ DAY 1.py
@@ -6,6 +6,8 @@ print(1)               # Tells the computer to say 1
 print(2)               # Tells the computer to say 2
 print(3)               # Tells the computer to say 3
 
-# This loop counts from 1 to 5
+# This loop counts from 1to5
 for number in range(1, 6):
 	print(number)
+
+print("end of the world")
